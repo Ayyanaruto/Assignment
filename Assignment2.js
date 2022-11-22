367124488af5ea2b1c4f03efc9db53f4d22cb7d4
Ayyanaruto <ayyan.shaikh.narotu@gmail.com>
2022-11-22 16:44:24 +0530

    Assignment1 & Assignment2 completed

--- Assignment2.js
+++ Assignment2.js
@@ -3,15 +3,20 @@
 // Using axios for fetching data is always preferred
 const axios=require('axios')
 let survey={}
+
+// Asynchronous function to get data from public api 
  const fetch=async()=>{
     const res=await axios.get("https://gorest.co.in/public/v2/users")
     
     const data=res.data
+
+    //Looping on reponse data and counting the fequency of active,inactive,male,female users
     for(let val of data){
         survey[val.status]=(survey[val.status]|| 0)+1
         survey[val.gender]=(survey[val.gender]|| 0)+1
         
     }
+    //Output
    console.log(survey)
  
  }
